18407aaa161e23b5747cf1d65d470cbf33a3d66b
mimidios <masterjimm8@gmail.com>
2022-08-23 20:15:03 +0300

    input grabber

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,34 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_system(hello_world)
+        .add_plugins(DefaultPlugins)
+        .add_system(input_grabber)
         .run();
 }
 
-fn hello_world() {
-    println!("hello world!");
+fn input_grabber(input: Res<Input<KeyCode>>)
+{
+    let mut x = 0;
+    let mut y = 0;
+
+    if input.pressed(KeyCode::A)
+    {
+        x = -1
+    }
+    else if input.pressed(KeyCode::D)
+    {
+        x = 1
+    }
+
+    if input.pressed(KeyCode::W)
+    {
+        y = 1
+    }
+    else if input.pressed(KeyCode::S)
+    {
+        y = -1
+    }
+
+    println!("{}", x);
+    print!("{}", y)
 }
