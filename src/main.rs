2caa88005f14978dc4f26517d7d1d720107c7d98
mimidios <masterjimm8@gmail.com>
2022-08-23 21:12:09 +0300

    speed normalization

--- src/main.rs
+++ src/main.rs
@@ -9,25 +9,31 @@ fn main() {
 
 fn input_grabber(input: Res<Input<KeyCode>>)
 {
-    let mut x = 0;
-    let mut y = 0;
+    let mut x: i32 = 0;
+    let mut y: i32 = 0;
 
     if input.pressed(KeyCode::A)
     {
-        x = -1
+        x = -256
     }
     else if input.pressed(KeyCode::D)
     {
-        x = 1
+        x = 256
     }
 
     if input.pressed(KeyCode::W)
     {
-        y = 1
+        y = 256
     }
     else if input.pressed(KeyCode::S)
     {
-        y = -1
+        y = -256
+    }
+
+    if x.abs() == 256 && y.abs() == 256
+    {
+        x = (x as f32 * 0.70710677) as i32;
+        y = (y as f32 * 0.70710677) as i32;	
     }
 
     println!("{}", x);
