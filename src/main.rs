14ac7b3037b8bd627c2dc2dd88e98b9b84fbff9d
mimidios <masterjimm8@gmail.com>
2022-08-24 20:06:58 +0300

    bevy_flycam implementation

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,20 @@
 use bevy::prelude::*;
+use bevy_flycam::PlayerPlugin;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugin(PlayerPlugin)
+        /*.insert_resource(MovementSettings
+        {
+            sensitivity: 0.00015, // default: 0.00012
+            speed: 12.0, // default: 12.0
+        })*/
         .add_system(game)
         .run();
 }
 
 fn game(input: Res<Input<KeyCode>>)
 {
-    let mut moving_angle: f64 = 0.0;
-    let mut is_moving = true;
 
-    if input.pressed(KeyCode::A)
-    {
-        if input.pressed(KeyCode::W)
-        {
-            moving_angle = 45.0;
-        }
-        else if input.pressed(KeyCode::S)
-        {
-            moving_angle = 135.0;
-        }
-        else
-        {
-            moving_angle = 90.0;
-        }   
-    }
-    else if input.pressed(KeyCode::D)
-    {
-        if input.pressed(KeyCode::W)
-        {
-            moving_angle = 315.0;
-        }
-        else if input.pressed(KeyCode::S)
-        {
-            moving_angle = 225.0;
-        }
-        else
-        {
-            moving_angle = 270.0;
-        }
-    }
-    else
-    {
-        if input.pressed(KeyCode::W)
-        {
-            moving_angle = 0.0;
-        }
-        else if input.pressed(KeyCode::S)
-        {
-            moving_angle = 180.0;
-        }
-        else
-        {
-            is_moving = false
-        }
-    }
-
-    let direction = moving_angle.to_radians().sin_cos();
-    println!("{}", direction.0);
-    print!("{}", direction.1);
-    print!("              ");
 }
